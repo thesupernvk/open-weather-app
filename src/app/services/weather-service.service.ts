2bf673433b0cbcabf7847eee331805aa4a24bb2e
Navin Vijaykumar <38689367+thesupernvk@users.noreply.github.com>
2018-09-28 17:20:37 +0200

    Second commit

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -10,7 +10,7 @@ export class WeatherService {
   //ID 0d3043a3816880d0b0a632c0d1fc8962
   //API call: http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
   
-  private URL_LOCATION = "http://api.openweathermap.org/data/2.5/weather";
+  private URL_LOCATION = "https://api.openweathermap.org/data/2.5/weather";
   private URL_LOCATION_MOCK = "http://localhost:3000/payload";
 
   constructor(private http: HttpClient) { }
